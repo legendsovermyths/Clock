430681ad9cfcbdaf9a250c9dcd615e740ff1f2e7
Mask Wolf <maskwolf2000@gmail.com>
2021-01-03 01:33:20 +0530

    refactor

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+function App() {
+  let Time = new Date().toLocaleTimeString();
+  var [time, setTime] = useState(Time);
+  setInterval(function () {
+    setTime((time) => {
+      time = new Date().toLocaleTimeString();
+      return time;
+    });
+  }, 1000);
+
+  return (
+    <div className='container'>
+      <h1>{time}</h1>
+      <button onClick={() => setTime((time = new Date().toLocaleTimeString()))}>
+        Get Time
+      </button>
+    </div>
+  );
+}
+
+export default App;
